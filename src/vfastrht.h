693ddedca0875d3646d5572c031d63f63a2c3a08
dedmanwalking <dedmanwalking@proton.me>
2026-08-03 20:28:31 +0000

    Add files via upload

--- src/vfastrht.h
+++ src/vfastrht.h
@@ -25,16 +25,27 @@
 # define VFASTRHT_KEY_LENGTH sizeof(int)
 #endif
 
-typedef struct vfastrhtS vfastrhtT;
+typedef struct vfastrhtS vfastrhtT;                                                                 // counterless rht.
+typedef struct vfastrht_ctrS vfastrht_ctrT;                                                         // counterful rht.
 typedef VFASTRHT_KEY_UNDERLYING_TYPE vfastrht_keyT;
 
+// counterless rht.
 vfastrhtT* vfastrht_table_init();                                                                   // initialises table.
 void vfastrht_table_destroy(vfastrhtT* const restrict table);                                       // destroys table.
 bool vfastrht_del(vfastrhtT* restrict table, const vfastrht_keyT* const restrict key);              // deletes entry from table, returns true if successful.
 bool vfastrht_put(vfastrhtT* restrict table, const vfastrht_keyT* const restrict key);              // puts entry in table, returns true if successful.
 bool vfastrht_search(vfastrhtT* restrict table, const vfastrht_keyT* const restrict key);           // searches entry within table, returns true if successful.
 
+// counterful rht.
+vfastrht_ctrT* vfastrht_ctr_table_init();
+void vfastrht_ctr_table_destroy(vfastrht_ctrT* const restrict table);                               // destroys table.
+bool vfastrht_ctr_del(vfastrht_ctrT* restrict table, const vfastrht_keyT* const restrict key);      // deletes entry from table, returns true if successful.
+bool vfastrht_ctr_put(vfastrht_ctrT* restrict table, const vfastrht_keyT* const restrict key);      // puts entry in table, returns true if successful.
+bool vfastrht_ctr_search(vfastrht_ctrT* restrict table, const vfastrht_keyT* const restrict key);   // searches entry within table, returns true if successful.
+
 # ifdef VFASTRHT_IMPLEMENTATION
+
+// counterless implementation.
 
 struct vfastrhtS{
     struct vfastrhtS* restrict under;
@@ -49,8 +60,10 @@ vfastrhtT* vfastrht_table_init(){
 }
 
 void vfastrht_table_destroy(vfastrhtT* const restrict table){
-    for(size_t i = 0; i < VFASTRHT_TABLE_SIZE; i++)
+    for(size_t i = 0; i < VFASTRHT_TABLE_SIZE; i++){
+        if(table[i].key) free(table[i].key);
         if(table[i].under) vfastrht_table_destroy(table[i].under);
+    }
     free(table);
 }
 
@@ -63,13 +76,15 @@ bool vfastrht_del(vfastrhtT* restrict table, const vfastrht_keyT* const restrict
 
     while(r < VFASTRHT_KEY_LENGTH){
         const size_t idx = vfastrht__idx(key, r);
-        if(!memcmp(table[idx].key, key, VFASTRHT_KEY_LENGTH)){
+        if(table[idx].key && !memcmp(table[idx].key, key, VFASTRHT_KEY_LENGTH)){
             free(table[idx].key);
             table[idx].key = NULL;
             return true;
         }
         if(!table[idx].under) return false;
         table = table[idx].under;
+
+        r++;
     }
 
     return false;
@@ -90,6 +105,8 @@ bool vfastrht_put(vfastrhtT* restrict table, const vfastrht_keyT* const restrict
         if(!table[idx].under)
             table[idx].under = vfastrht_table_init();
         table = table[idx].under;
+
+        r++;
     }
 
     return false;
@@ -100,11 +117,95 @@ bool vfastrht_search(vfastrhtT* restrict table, const vfastrht_keyT* const restr
 
     while(r < VFASTRHT_KEY_LENGTH){
         const size_t idx = vfastrht__idx(key, r);
-        if(!memcmp(table[idx].key, key, VFASTRHT_KEY_LENGTH))
+        if(table[idx].key && !memcmp(table[idx].key, key, VFASTRHT_KEY_LENGTH))
             return true;
         if(!table[idx].under)
             return false;
         table = table[idx].under;
+        
+        r++;
+    }
+
+    return false;
+}
+
+// counterful implementation.
+
+struct vfastrht_ctrS{
+    struct vfastrht_ctrS* restrict under;
+    vfastrht_keyT* restrict key;
+    uint64_t ctr;
+};
+
+vfastrht_ctrT* vfastrht_ctr_table_init(){
+    return (vfastrht_ctrT*)calloc(VFASTRHT_TABLE_SIZE, sizeof(vfastrht_ctrT));
+}
+
+void vfastrht_ctr_table_destroy(vfastrht_ctrT* const restrict table){
+    for(size_t i = 0; i < VFASTRHT_TABLE_SIZE; i++){
+        if(table[i].key) free(table[i].key);
+        if(table[i].under) vfastrht_ctr_table_destroy(table[i].under);
+    }
+    free(table);
+}
+
+bool vfastrht_ctr_del(vfastrht_ctrT* restrict table, const vfastrht_keyT* const restrict key){
+    unsigned short r = 0;       // why short? because we don't need int! really both short & int work.
+
+    while(r < VFASTRHT_KEY_LENGTH){
+        const size_t idx = vfastrht__idx(key, r);
+
+        if(table[idx].key && !memcmp(table[idx].key, key, VFASTRHT_KEY_LENGTH) && --table[idx].ctr){
+            free(table[idx].key);
+            table[idx].key = NULL;
+            return true;
+        }
+        if(!table[idx].under) return false;
+        table = table[idx].under;
+        
+        r++;
+    }
+
+    return false;
+}
+
+bool vfastrht_ctr_put(vfastrht_ctrT* restrict table, const vfastrht_keyT* const restrict key){
+    unsigned short r = 0;
+
+    while(r < VFASTRHT_KEY_LENGTH){
+        const size_t idx = vfastrht__idx(key, r);
+        if(!table[idx].key){
+            table[idx].key = malloc(VFASTRHT_KEY_LENGTH);
+            memcpy(table[idx].key, key, VFASTRHT_KEY_LENGTH);
+            table[idx].ctr = 1;
+            return true;
+        }
+        if(!memcmp(table[idx].key, key, VFASTRHT_KEY_LENGTH)){
+            table[idx].ctr++;
+            return false;
+        }
+        if(!table[idx].under)
+            table[idx].under = vfastrht_ctr_table_init();
+        table = table[idx].under;
+
+        r++;
+    }
+
+    return false;
+}
+
+bool vfastrht_ctr_search(vfastrht_ctrT* restrict table, const vfastrht_keyT* const restrict key){
+        unsigned short r = 0;
+
+    while(r < VFASTRHT_KEY_LENGTH){
+        const size_t idx = vfastrht__idx(key, r);
+        if(table[idx].key && !memcmp(table[idx].key, key, VFASTRHT_KEY_LENGTH))
+            return true;
+        if(!table[idx].under)
+            return false;
+        table = table[idx].under;
+
+        r++;
     }
 
     return false;
